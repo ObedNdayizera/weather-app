53bf1984200b3d5fc03662d570e6f3b054628858
ObedNdayizera <obedndayizera@gmail.com>
2023-06-01 00:06:46 +0200

    pushing weather component style changes

---
+++ src/assets/styles/weather.ts
@@ -0,0 +1,27 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  container: {
+    background: "#0E0F1C",
+    borderRadius: "25px",
+  },
+  temperatures: {
+    overflowX: "scroll",
+    width: "550px",
+    boxShadow: "",
+    "&::-webkit-scrollbar": {
+      display: "none",
+      overflowX: "scroll",
+    },
+  },
+  temperature: {
+    borderRadius: 25,
+    background: "#b8dfff",
+    alignItems: 'center',
+    minWidth: "65px",
+    padding: "18px 0px",
+    color: "rgba(7, 10, 51)",
+  },
+});
+
+export default useStyles;
--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -2,14 +2,16 @@ import { Stack, Box, Typography } from "@mui/material";
 import CloudIcon from "@mui/icons-material/Cloud";
 
 import { images } from "../../assets/images";
+import useStyles from "../../assets/styles/weather";
 
 function weather() {
+  const classes = useStyles();
   return (
     <>
       <Stack
         p={4}
         spacing={2.5}
-        sx={{ background: "#0E0F1C", borderRadius: "25px" }}
+        className={classes.container}
       >
         <Box gap={5} sx={{ display: "flex" }}>
           <img style={{ marginTop: "-25px" }} width={110} src={images.sun} />
@@ -38,248 +40,116 @@ function weather() {
             </Typography>
           </Box>
         </Box>
-        <Box gap={1} sx={{ display: "flex", overflowX: 'scroll', width: '550px', boxShadow: ''}}>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '18px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+        <Stack direction={'row'} gap={1} className={classes.temperatures}>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-          <Box
-            borderRadius={5}
-            sx={{
-              background: "#b8dfff",
-              display: "grid",
-              justifyItems: "center",
-              minWidth: "65px",
-              padding: '15px 0px'
-            }}
-            justifyContent={"center"}
-            color="rgba(7, 10, 51)"
-          >
-            <Typography fontSize={13} variant="body1" color="rgba(7, 10, 51)">
+          </Stack>
+          <Stack spacing={0.5} className={classes.temperature}>
+            <Typography fontSize={13} variant="body1" >
               09 am
             </Typography>
             <CloudIcon  style={{ color: "#0E0F1C"}}/>
-            <Typography variant="body1" fontSize={13} fontWeight={'bold'} color="rgba(7, 10, 51)">
+            <Typography variant="body1" fontSize={13} fontWeight={'bold'}>
               17
             </Typography>
-          </Box>
-        </Box>
+          </Stack>
+        </Stack>
       </Stack>
     </>
   );
