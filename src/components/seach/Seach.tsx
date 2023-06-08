1c0d9ea8f018a129c8cc3176360321916bfea57d
ObedNdayizera <obedndayizera@gmail.com>
2023-06-08 23:50:23 +0200

    start working on seach bar component

---
+++ src/components/seach/Seach.tsx
@@ -0,0 +1,13 @@
+import { Paper } from '@mui/material';
+
+function Seach() {
+  return (
+    <>
+        <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+
+        </Paper>
+    </>
+  )
+}
+
+export default Seach;
